43cf9ddac0e4d40b5e847b33e89abefa99c55f4d
intastellar <70743293+intastellar@users.noreply.github.com>
2020-12-07 19:51:11 +0100

    Tilføjet CTA Knappen

--- js/calendar.js
+++ js/calendar.js
@@ -3,27 +3,31 @@ let days = 25;
 
 for(let i = 1; i<days; i++){
     let door = document.createElement("article");
+    let door_face = document.createElement("section");
     let door_front = document.createElement("section");
     let door_back = document.createElement("section");
 
     door.setAttribute("class", "door");
-    door_back.setAttribute("class", "door-back");
-    door_front.setAttribute("class", "door-front");
+    door_face.setAttribute("class", "door__face");
+    door_back.setAttribute("class", "door__back");
+    door_front.setAttribute("class", "door__front");
     
     door_front.innerHTML = i;
-
-    door.appendChild(door_front.cloneNode(true));
-    door.appendChild(door_back.cloneNode(true));
+    door_face.appendChild(door_front);
+    door_face.appendChild(door_back);
+    door.appendChild(door_face.cloneNode(true));
 
     c_container.appendChild(door);
 }
-
-let doors = document.querySelectorAll(".door");
-let front = document.querySelectorAll("door-front");
+let doors = document.querySelectorAll(".door__face");
+let front = document.querySelectorAll(".door__front");
 
 for(var i = 0; i<doors.length; i++){
     doors[i].addEventListener("mouseover", function(){
-        console.log(front);
-        front[i].style.transfrom = "rotateZ(10deg)";
+        this.style.transform = "rotateY(180deg)";
+    });
+
+    doors[i].addEventListener("mouseout", function(){
+        this.style.transform = "";
     });
 }
