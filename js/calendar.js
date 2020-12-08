d7cc263b4838a2cb7c2fd83fde1954d06377a9a0
intastellar <70743293+intastellar@users.noreply.github.com>
2020-12-08 12:35:40 +0100

    Random Calendar

--- js/calendar.js
+++ js/calendar.js
@@ -93,3 +93,31 @@ for(var i = 0; i<cta.length; i++){
         document.querySelector(".overlay").style.visibility = "visible";
     });
 }
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+let d = Array.prototype.slice.call(document.querySelectorAll(".door"));
+
+shuffle(d);
+
+for(var j=0; j<d.length; j++){
+    console.log(d[j]);
+    c_container.appendChild(d[j]);
+}
